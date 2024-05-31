128011e968c1831fa9e7fb2f4f7e227f28305045
Jens Reidel <adrian@travitia.xyz>
2024-06-01 00:17:16 +0200

    config: Add common config for book-style foldables
    Book-style foldables like the Pixel Fold, Galaxy Z Fold and Xiaomi MIX
    Fold series have two displays: one primary, near tablet-sized inner
    panel and a smaller, phone-sized outer panel.
    
    These devices need a combination of configs to enable both phone- and
    tablet-targeted features. Therefore, add a config file merging contents
    of the common phone and tablet configs.
    
    Change-Id: Ibf89e4336fa119ebfb79aad8dac4266fd4972985
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ config/common_full_foldable_book_telephony.mk
@@ -0,0 +1,11 @@
+# Inherit mobile full common Aurora stuff
+$(call inherit-product, vendor/aurora/config/common_mobile_full.mk)
+
+# Enable support of one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode?=true
+
+# Inherit tablet common Aurora stuff
+$(call inherit-product, vendor/aurora/config/tablet.mk)
+
+$(call inherit-product, vendor/aurora/config/telephony.mk)
