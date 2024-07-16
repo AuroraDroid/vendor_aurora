49d581125b871b2ba32aa83c1978f5f5c826ae59
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-16 12:33:36 +0000

    vendor: include packages.mk

--- config/common.mk
+++ config/common.mk
@@ -189,3 +189,4 @@ CUSTOM_LOCALES += \
 include vendor/aurora/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+$(call inherit-product-if-exists, vendor/aurora/config/packages.mk)
