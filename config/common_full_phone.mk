LineageOS/android_vendor_lineage

config: Rename mobile-specific configs
Since common_full.mk and common_mini.mk are mobile-specific, rename them

Change-Id: I2c47fd0688350ac815ccecbff7b453d7fab5d5ea

config/common_full_phone.mk
@@ -1,5 +1,5 @@
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+# Inherit mobile full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

AuroraDroid/vendor_aurora

config/common_full_phone.mk
@@ -1,5 +1,5 @@
 # Inherit full common Aurora stuff
-$(call inherit-product, vendor/aurora/config/common_full.mk)
+$(call inherit-product, vendor/aurora/config/common_mobile_full.mk)
 
 #Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \