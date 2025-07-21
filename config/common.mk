474cc566bd3e24a9fbc14b48b51de27cc9808f2e
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,9 @@ PRODUCT_BRAND ?= AuroraDroid
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
